e26393579124943bb0e159499d2c4ec0643c2e77
Marito <mario@Mario-LT.lan>
2025-01-24 17:36:52 +0000

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Web-Personal/', // Reemplaza "tu-repositorio" con el nombre de tu repositorio
   plugins: [react()],
 })
